a8b2631efd33b8012110679f2fde4f8c85b58914
Axel Laget <axel.laget@gmail.com>
2022-08-02 19:58:36 +0200

    feat: auth
    feat : auth

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,11 +1,12 @@
-import Head from 'next/head';
-import { CacheProvider } from '@emotion/react';
-import LocalizationProvider from '@mui/lab/LocalizationProvider';
-import AdapterDateFns from '@mui/lab/AdapterDateFns';
-import { CssBaseline } from '@mui/material';
-import { ThemeProvider } from '@mui/material/styles';
-import { createEmotionCache } from '../utils/create-emotion-cache';
-import { theme } from '../theme';
+import Head from "next/head";
+import { CacheProvider } from "@emotion/react";
+import LocalizationProvider from "@mui/lab/LocalizationProvider";
+import AdapterDateFns from "@mui/lab/AdapterDateFns";
+import { CssBaseline } from "@mui/material";
+import { ThemeProvider } from "@mui/material/styles";
+import { createEmotionCache } from "../utils/create-emotion-cache";
+import { theme } from "../theme";
+import { SessionProvider } from "next-auth/react";
 
 const clientSideEmotionCache = createEmotionCache();
 
@@ -16,21 +17,18 @@ const App = (props) => {
 
   return (
     <CacheProvider value={emotionCache}>
-      <Head>
-        <title>
-          Material Kit Pro
-        </title>
-        <meta
-          name="viewport"
-          content="initial-scale=1, width=device-width"
-        />
-      </Head>
-      <LocalizationProvider dateAdapter={AdapterDateFns}>
-        <ThemeProvider theme={theme}>
-          <CssBaseline />
-          {getLayout(<Component {...pageProps} />)}
-        </ThemeProvider>
-      </LocalizationProvider>
+      <SessionProvider session={pageProps.session}>
+        <Head>
+          <title>Material Kit Pro</title>
+          <meta name="viewport" content="initial-scale=1, width=device-width" />
+        </Head>
+        <LocalizationProvider dateAdapter={AdapterDateFns}>
+          <ThemeProvider theme={theme}>
+            <CssBaseline />
+            {getLayout(<Component {...pageProps} />)}
+          </ThemeProvider>
+        </LocalizationProvider>
+      </SessionProvider>
     </CacheProvider>
   );
 };
--- src/pages/sites.js
+++ src/pages/sites.js
@@ -1,5 +1,4 @@
 import Head from "next/head";
-import { Box, Container, Grid } from "@mui/material";
 import { Budget } from "../components/dashboard/budget";
 import { LatestOrders } from "../components/dashboard/latest-orders";
 import { LatestProducts } from "../components/dashboard/latest-products";
@@ -12,14 +11,59 @@ import { DashboardLayout } from "../components/dashboard-layout";
 
 import Sites from "src/components/sites";
 
-const Dashboard = () => (
-  <>
-    <Container maxWidth={false}>
-      <Sites />
-    </Container>
-  </>
-);
+import { Box, Button, Container, Grid, Paper, Stack, Typography, IconButton } from "@mui/material";
+import { useSession, signIn, signOut } from "next-auth/react";
+import LogoutIcon from "@mui/icons-material/Logout";
 
+const Dashboard = () => {
+  const { data: session } = useSession();
+  if (session) {
+    if (session.user.email == "axel.laget@gmail.com") {
+      return (
+        <>
+          <Stack direction="row" justifyContent="flex-end" alignItems="center" spacing={3}>
+            <Typography color="primary"> {session.user.email}</Typography>
+            <IconButton color="primary" onClick={() => signOut()}>
+              <LogoutIcon fontSize="large"></LogoutIcon>
+            </IconButton>
+          </Stack>
+          <Head>
+            <title>Dendro</title>
+          </Head>
+          <Box
+            component="main"
+            sx={{
+              flexGrow: 1,
+              py: 8,
+            }}
+          >
+            <Container maxWidth={false}>
+              <Sites />
+            </Container>
+          </Box>
+        </>
+      );
+    }
+    return (
+      <Stack direction="row" justifyContent="flex-end" alignItems="center" spacing={3}>
+        <Typography color="error"> You are not authorized {session.user.email}</Typography>
+        <IconButton color="error" onClick={() => signOut()}>
+          <LogoutIcon fontSize="large"></LogoutIcon>
+        </IconButton>
+      </Stack>
+    );
+  }
+  return (
+    <>
+      <Stack direction="column" justifyContent="center" alignItems="center" spacing={4}>
+        <Typography variant="h4">Not signed in</Typography>
+
+        <Button size="large" variant="outlined" onClick={() => signIn()}>
+          Sign in
+        </Button>
+      </Stack>
+    </>
+  );
+};
 Dashboard.getLayout = (page) => <DashboardLayout>{page}</DashboardLayout>;
-
 export default Dashboard;
