018b723b5c00bbe1c9b04f1ec31541184aa933dd
Axel Laget <axel.laget@gmail.com>
2022-08-02 19:58:36 +0200

    feat: auth
    feat : auth

--- src/pages/import.js
+++ src/pages/import.js
@@ -1,5 +1,4 @@
 import Head from "next/head";
-import { Box, Container, Grid } from "@mui/material";
 import { Budget } from "../components/dashboard/budget";
 import { LatestOrders } from "../components/dashboard/latest-orders";
 import { LatestProducts } from "../components/dashboard/latest-products";
@@ -9,17 +8,60 @@ import { TotalCustomers } from "../components/dashboard/total-customers";
 import { TotalProfit } from "../components/dashboard/total-profit";
 import { TrafficByDevice } from "../components/dashboard/traffic-by-device";
 import { DashboardLayout } from "../components/dashboard-layout";
-
 import Import from "src/components/import";
+import { Box, Button, Container, Grid, Paper, Stack, Typography, IconButton } from "@mui/material";
+import { useSession, signIn, signOut } from "next-auth/react";
+import LogoutIcon from "@mui/icons-material/Logout";
 
-const Dashboard = () => (
-  <>
-    <Container maxWidth={false}>
-      <Import />
-    </Container>
-  </>
-);
+const Dashboard = () => {
+  const { data: session } = useSession();
+  if (session) {
+    if (session.user.email == "axel.laget@gmail.com") {
+      return (
+        <>
+          <Stack direction="row" justifyContent="flex-end" alignItems="center" spacing={3}>
+            <Typography color="primary"> {session.user.email}</Typography>
+            <IconButton color="primary" onClick={() => signOut()}>
+              <LogoutIcon fontSize="large"></LogoutIcon>
+            </IconButton>
+          </Stack>
+          <Head>
+            <title>Dendro</title>
+          </Head>
+          <Box
+            component="main"
+            sx={{
+              flexGrow: 1,
+              py: 8,
+            }}
+          >
+            <Container maxWidth={false}>
+              <Import />
+            </Container>
+          </Box>
+        </>
+      );
+    }
+    return (
+      <Stack direction="row" justifyContent="flex-end" alignItems="center" spacing={3}>
+        <Typography color="error"> You are not authorized {session.user.email}</Typography>
+        <IconButton color="error" onClick={() => signOut()}>
+          <LogoutIcon fontSize="large"></LogoutIcon>
+        </IconButton>
+      </Stack>
+    );
+  }
+  return (
+    <>
+      <Stack direction="column" justifyContent="center" alignItems="center" spacing={4}>
+        <Typography variant="h4">Not signed in</Typography>
 
+        <Button size="large" variant="outlined" onClick={() => signIn()}>
+          Sign in
+        </Button>
+      </Stack>
+    </>
+  );
+};
 Dashboard.getLayout = (page) => <DashboardLayout>{page}</DashboardLayout>;
-
 export default Dashboard;
